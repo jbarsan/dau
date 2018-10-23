f04e7c3ec3c44a901d19297dd38b2445a84ad5c3
João Carlos Barsan <jbarsan@yandex.com>
2018-10-23 17:54:48 -0300

    criando templates

--- suporte/urls.py
+++ suporte/urls.py
@@ -2,6 +2,13 @@ from django.urls import include, path
 from suporte import views as suporte_views
 
 urlpatterns = [
-    path('entrada/<int:pk>/', suporte_views.entradas, name='entradas'),
+    path('entrada/', suporte_views.entradas, name='entradas'),
+    path('entrada/<int:pk>/', suporte_views.list_entradas, name='list_entradas'),
+    #path('entrada/<int:pk>/novo_atendimento', suporte_views.novo_atendimento, name='novo_atendimento'),
+    path('equipamento/', suporte_views.equipamentos, name='equipamentos'),
+    path('equipamento/<int:pk>/', suporte_views.list_equipamentos, name='list_equipamentos'),
+    #path('equipamento/<int:pk>/novo/', suporte_views.novo_equipamento, name='novo_equipamento'),
+    path('departamento/', suporte_views.departamentos, name='departamentos'),
+    path('departamento/<int:pk>/', suporte_views.list_departamentos, name='list_departamentos'),
 
 ]
--- suporte/views.py
+++ suporte/views.py
@@ -4,12 +4,48 @@ from suporte.models import *
 
 # Create your views here.
 def home(request):
+    return render(request, 'home.html')
+
+
+def entradas(request):
     entradas = Entrada.objects.all()
+    return render(request, 'suporte/entradas.html', {'entradas': entradas})
 
-    return render(request, 'home.html', {'entradas': entradas})
+
+def list_entradas(request, pk):
+    get_entrada = get_object_or_404(Entrada, pk=pk)
+    return render(request, 'suporte/entrada.html', {'get_entrada': get_entrada})
 
 
-def entradas(request, pk):
-    # entradas = Entrada.objects.get(pk=pk)
-    entradas = get_object_or_404(Entrada, pk=pk)
-    return render(request, 'suporte/entrada.html', {'entradas': entradas})
+def atendimentos(request):
+    atendimentos = Atendimento.objects.all()
+    return
+
+def novo_atendimento(request, pk):
+    atendimento = get_object_or_404(Atendimento, pk=pk)
+    return render(request, 'suporte/novo_atendimento.html', {'atendimento': atendimento})
+
+
+def equipamentos(request):
+    equips = Equipamento.objects.all()
+    return render(request, 'suporte/equipamentos.html', {'equipamentos': equips})
+
+
+def list_equipamentos(request, pk):
+    get_equipamento = get_object_or_404(Equipamento, pk=pk)
+    return render(request, 'suporte/equipamento.html', {'get_equipamento': get_equipamento})
+
+
+def novo_equipamento(request, pk):
+    equipamento = get_object_or_404(Equipamento, pk=pk)
+    return render(request, 'suporte/novo_equipamento.html', {'equipamento': equipamento})
+
+
+def departamentos(request):
+    departamentos = Departamento.objects.all()
+    return render(request, 'suporte/departamentos.html', {'departamentos': departamentos})
+
+
+def list_departamentos(request, pk):
+    get_departamento = get_object_or_404(Departamento, pk=pk)
+    return render(request, 'suporte/departamento.html', {'get_departamentos': get_departamento})
